4b4f302f72f9131f58457d85effbbd469ec2f252
charliejl28 <cmj3@princeton.edu>
2016-05-08 14:59:23 -0400

    updated app info

--- config/env/default.js
+++ config/env/default.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    title: 'MyMoments',
+    description: 'Custom news stories based on Twitter interests',
+    keywords: 'Twitter, moments, news, personalized, custom',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   port: process.env.PORT || 3000,
